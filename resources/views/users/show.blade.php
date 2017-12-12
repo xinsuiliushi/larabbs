0478f994e876cd04b762d2b247da631141e40591
piao <phoenixljj0823@gmail.com>
2017-12-12 16:37:11 +0000

    List topics created by user

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -28,9 +28,11 @@
         <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
             <div class="panel panel-default">
                 <div class="panel-body">
-                <span>
-                    <h1 class="panel-title pull-left" style="font-size:30px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
-                </span>
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a href="#">Ta 的话题</a></li>
+                        <li><a href="#">Ta 的回复</a></li>
+                    </ul>
+                    @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
                 </div>
             </div>
             <hr>
