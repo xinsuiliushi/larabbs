3e363b6f45b400f6033f5d644d3d0938eb236de7
piao <phoenixljj0823@gmail.com>
2017-12-18 16:15:47 +0000

    Admin permissions

--- config/administrator.php
+++ config/administrator.php
@@ -104,7 +104,7 @@ return array(
      *
      * @type string
      */
-    'home_page' => 'users',
+    'home_page' => 'topics',
 
     /*
      * The route to which the user will be taken when they click the "back to site" button
@@ -125,7 +125,7 @@ return array(
      *
      * @type string
      */
-    'logout_path' => false,
+    'logout_path' => 'permission-denied',
 
     /*
      * This is the key of the return path that is sent with the redirection to your login_action. Session::get('redirect') will hold the return URL.
